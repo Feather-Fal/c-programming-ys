a6f7d8de44406e8af8ee8d72570c81ccff2bc276
Feather-Fal <248389723+Feather-Fal@users.noreply.github.com>
2026-05-12 22:13:23 +0800

    add: 第一章例1.1 初识c程序示例代码

--- capter/第一章 程序设计概述/1.5 初识C程序/例1.1-hello.c
+++ capter/第一章 程序设计概述/1.5 初识C程序/例1.1-hello.c
@@ -1,1 +1,9 @@
+/* 例1.1 编写程序，在屏幕上输出 Welcome to C 和 Programming is fun。 */
 
+#include <stdio.h>
+int main(void)
+{
+    printf("Welcome to C\n");
+    printf("Programming is fun\n");
+    return 0;
+}
